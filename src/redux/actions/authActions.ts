e3babaac0b0eb01550886a42fc52fea46fc51465
Flovet-stack <lovetofloveto3@gmail.com>
2023-11-29 11:41:40 +0100

    factored the login action

--- src/redux/actions/authActions.ts
+++ src/redux/actions/authActions.ts
@@ -1,26 +1,29 @@
-import { AuthDto, SuccessLoginDto } from '@instanvi/client';
+import { SuccessLoginDto } from '@instanvi/client';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { AxiosResponse } from 'axios';
 
 import { ILoginFormData } from '@/@types';
 import { INSTANVI_STORAGE_ACCESS, INSTANVI_STORAGE_USER } from '@/constants';
 import CryptoStorageHelper from '@/helpers/cryptoStorageHelper';
 import { AuthAPI } from '@/libs/instanvi-service';
 
-export const loginThunk = createAsyncThunk(
+interface LoginThunkResponse {
+  user: SuccessLoginDto['user'];
+  access: SuccessLoginDto['access'];
+}
+
+export const loginThunk = createAsyncThunk<LoginThunkResponse, ILoginFormData>(
   'login',
   async (data: ILoginFormData) => {
     try {
-      const login = (data: AuthDto): Promise<AxiosResponse> => {
-        return AuthAPI.login(data);
-      };
-      const res = await login(data);
-      const responseData: SuccessLoginDto = res.data.data;
+      const response = await AuthAPI.login(data);
+      const responseData: SuccessLoginDto = response.data.data;
       const { user, access } = responseData;
+
       // save user and access to local storage
       CryptoStorageHelper.encryptAndSave(INSTANVI_STORAGE_USER, user);
       CryptoStorageHelper.encryptAndSave(INSTANVI_STORAGE_ACCESS, access);
-      return responseData;
+
+      return { user, access };
     } catch (error) {
       return Promise.reject(error);
     }
