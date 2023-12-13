bfcd557128403d34aec0894519776ac43c627117
Flovet-stack <lovetofloveto3@gmail.com>
2023-12-13 01:26:52 +0100

    fixing linting issues

--- src/api/persona/useCreatePersona.ts
+++ src/api/persona/useCreatePersona.ts
@@ -1,12 +1,12 @@
 import { useMutation } from '@tanstack/react-query'; // Importez useQueryClientimport { useQuery } from "react-query";
 import toast from 'react-hot-toast';
 
-import { PersonasAPI } from '@/libs/instanvi-service';
+// import { PersonasAPI } from '@/libs/instanvi-service';
 
 export const useCreatePersona = () => {
   const mutation = useMutation({
-    mutationKey: ['createPersona'],
-    mutationFn: PersonasAPI.createPersonas as unknown,
+    // mutationKey: ['createPersona'],
+    // mutationFn: (variables) => PersonasAPI.createPersonas(variables),
     onError: () => toast.error('An error has occurred !!!'),
   });
 
